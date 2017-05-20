a7f58163b645c6b8338ff31c3a9bc997bf0903b9
Mateusz <mateusz.okarmus@gmail.com>
2017-05-20 21:22:05 +0200

    Set up event store based on mysql

--- account-core/src/main/java/org/okarmus/accountcore/AccountCoreConfiguration.java
+++ account-core/src/main/java/org/okarmus/accountcore/AccountCoreConfiguration.java
@@ -1,13 +1,8 @@
 package org.okarmus.accountcore;
 
 import org.axonframework.common.IdentifierFactory;
-import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.File;
 
 @Configuration
 public class AccountCoreConfiguration {
@@ -15,11 +10,6 @@ public class AccountCoreConfiguration {
     @Bean
     IdentifierFactory identifierFactory() {
         return IdentifierFactory.getInstance();
-    }
-
-    @Bean
-    EventStore eventStore() {
-        return  new EmbeddedEventStore(new InMemoryEventStorageEngine());
     }
 
 }
