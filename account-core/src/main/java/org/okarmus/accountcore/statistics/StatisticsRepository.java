b4c7c47145fe6e05a0a8c6def527493833baa29f
okarmus <Mateusz.Okarmus@uk.tesco.com>
2017-05-31 21:40:01 +0200

    Statistics are stored in mondo db not in mysql

--- account-core/src/main/java/org/okarmus/accountcore/statistics/StatisticsRepository.java
+++ account-core/src/main/java/org/okarmus/accountcore/statistics/StatisticsRepository.java
@@ -1,10 +1,10 @@
 package org.okarmus.accountcore.statistics;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface StatisticsRepository extends JpaRepository<Statistic, String> {
+public interface StatisticsRepository extends MongoRepository<Statistic, String> {
 
     Optional<Statistic> findByDate(String date);
 
